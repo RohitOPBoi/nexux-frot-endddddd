4a35e9fda2e7949763e5bf510f2fcda46ab78ff2
RohitOPBoi <rohitvani07@gmail.com>
2026-09-19 17:15:00 +0530

    rebuild the front page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { LandingSections } from "@/components/landing/LandingSections";
+
+export default function Home() {
+  return (
+    <main className="w-full min-h-screen">
+      <LandingSections />
+    </main>
+  );
+}
